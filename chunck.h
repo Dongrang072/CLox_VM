2475ed9e84d8de5f974cc2cb7200d98aa44f112d
Dongrang072 <robot4097@gmail.com>
2024-06-29 16:58:10 +0900

    feat bytecode and disassemlbing

---
+++ chunck.h
@@ -0,0 +1,24 @@
+#ifndef CLOX_CHUNCK_H
+#define CLOX_CHUNCK_H
+
+#include "common.h"
+#include "value.h"
+
+typedef enum {
+    OP_CONSTANT,
+    OP_RETURN,
+}OpCode;
+
+typedef struct { //dynamic Array
+    int count;
+    int capacity;
+    uint8_t* code;
+    int* lines;
+    ValueArray constants;
+}Chunk;
+
+void initChunk(Chunk* chunk);
+void freeChunk(Chunk* chunk);
+void writeChunk(Chunk* chunk, uint8_t byte, int line);
+int addConstant(Chunk* chunk, Value value);
+#endif //CLOX_CHUNCK_H
